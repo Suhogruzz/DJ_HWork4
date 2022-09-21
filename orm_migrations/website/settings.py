571683a6cd1d209252b54bb37662916ca0949e71
Suhogruzz <madyson123@yandex.ru>
2022-09-21 14:04:21 +0300

    Added forgotten migrations

--- orm_migrations/website/settings.py
+++ orm_migrations/website/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'USER': 'postgres',
-        'PASSWORD': 'ikzgybx',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
